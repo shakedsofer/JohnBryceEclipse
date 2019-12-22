0fbaee422e14506e65d934994f90d8b665ca98e2
Shaked Sofer <shakedsof@gmail.com>
2019-12-22 20:40:34 +0200

    FinishHomeWorkLec_02

--- MyFirstProject/src/HomeWorkLec_02.java
+++ MyFirstProject/src/HomeWorkLec_02.java
@@ -5,27 +5,31 @@ public class HomeWorkLec_02 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Give me double digit"); // ãå ñéôøúé
-		
+
 		int num = scan.nextInt();
-		int num02;
-		int num03;
-		num02 = num / 10 ;
 
-		num03 = num02  % num02;  
-	
-		if (num03 == num) 
-			
+		scan.close();
+
+		double num02;
+		double num03;
+		num02 = num / 10;
+
+		num03 = num % 10;
+
+		if (num03 == (int) num02) {
+
 			System.out.println("Equal");
-		}else if
 
-	{
-		System.out.println("Not equal");
+		} else if (num > 9 && num < 100) {
 
-	}else {
-		System.out.println("Invalid");	
-		
+			System.out.println("Not equal");
+
+		} else {
+		System.out.println("Invalid");
+
 		}
 	}
 
+}
